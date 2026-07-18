4a2adcdeeacee88736f934b6c9a1bb839a42b9be
vansh yadav <vanshydv886@gmail.com>
2026-07-18 22:27:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (21.75%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def subsets(self, nums):
+        n = len(nums)
+        total_subsets = 1 << n
+        result = []
+        for num in range(0, total_subsets):
+            lst = []
+            for i in range(0, n):
+                if num & (1 << i) != 0:
+                    lst.append(nums[i])
+
+            result.append(lst)
+        return result
+        
